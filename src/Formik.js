43c41776d287ab1b99cc2e7d5e164a8eb5060669
Nimith-M-Gowda <nimithbbz10@gmail.com>
2021-07-15 21:46:57 +0530

    chore(refactor): removed boiler plates and code is optimized

--- src/Formik.js
+++ src/Formik.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { useFormik } from "formik";
+// import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
 };
 
-const onSubmit = (event, values) => {
-  event.preventDefault();
+const onSubmit = (values) => {
   console.log("🚀 ~ file: formik.js ~ line 12 ~ formik ~ formik", values);
 };
 
@@ -39,71 +40,103 @@ const validationSchema = Yup.object({
   name: Yup.string().required("name is Required"),
   email: Yup.string()
     .required("email is required")
-    .email("enail format is incorrect"),
+    .email("email format is incorrect"),
   channel: Yup.string().required("channel name is required"),
 });
-function Formik() {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-    // validate,
-  });
+function FormikForm() {
+  //Removing Boiler plates by using Formik Context
+  // const formik = useFormik({
+  //   initialValues,
+  //   onSubmit,
+  //   validationSchema,
+  //   // validate,
+  // });
 
   return (
-    <div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {/*
+      //replacing with Form Component
       <form onSubmit={formik.handleSubmit}>
+      */}
+      <Form>
         <div>
           <label>Name</label>
-          <input
+          <Field
             type="text"
             name="name"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-            onBlur={formik.handleBlur}
+            //reducing boiler plates
+            // onChange={formik.handleChange}
+            // value={formik.values.name}
+            // onBlur={formik.handleBlur}
+
+            //removing afetr Field Component
+            // {...getFieldProps("name")}
           />
         </div>
+        {/* Introducing ErrorMessage
         <div>
           {formik.touched.name && formik.errors.name ? (
             <span>{formik.errors.name}</span>
           ) : null}
-        </div>
+        </div> */}
+        <ErrorMessage name="name" />
         <div>
           <label>Email</label>
-          <input
+          <Field
             type="email"
             name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            onBlur={formik.handleBlur}
+            // onChange={formik.handleChange}
+            // value={formik.values.email}
+            // onBlur={formik.handleBlur}
+
+            //removing afetr Field Component
+            // {...getFieldProps("email")}
           />
         </div>
+        {/*Introducing ErrorMessage
         <div>
           {formik.touched.email && formik.errors.email ? (
             <span>{formik.errors.email}</span>
           ) : null}
-        </div>
+        </div> */}
+        <ErrorMessage name="email" />
         <div>
           <label>Channel</label>
-          <input
-            type="text"
+          <Field
+            // as="textarea"
+            type="textarea"
             name="channel"
-            onChange={formik.handleChange}
-            value={formik.values.channel}
-            onBlur={formik.handleBlur}
+            // onChange={formik.handleChange}
+            // value={formik.values.channel}
+            // onBlur={formik.handleBlur}
+
+            //removing afetr Field Component
+            // {...getFieldProps("channel")}
           />
         </div>
+        {/* Introducing ErrorMessage
         <div>
           {formik.touched.channel && formik.errors.channel ? (
             <span>{formik.errors.channel}</span>
           ) : null}
+        </div> */}
+        <ErrorMessage name="channel" />
+
+        <div>
+          <label>comments</label>
+          <Field as="textarea" name="comments" />
         </div>
+
         <div>
           <button type="submit">Submit</button>
         </div>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   );
 }
 
-export default Formik;
+export default FormikForm;
